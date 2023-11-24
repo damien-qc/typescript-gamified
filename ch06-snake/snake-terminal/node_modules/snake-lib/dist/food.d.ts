4f7d49bb5847199ca9ec27ee1c3c54202730638f
Damien Demessence <damiend@4thwhale.com>
2023-11-24 13:18:47 -0500

    main: cleanup dist folders from ch06

--- ch06-snake/snake-terminal/node_modules/snake-lib/dist/food.d.ts
+++
@@ -1,6 +0,0 @@
-import type { GameObject, WorldBoundaries } from "./game";
-export type Food = GameObject & {
-    name: "food";
-};
-export declare function createFood(boundaries: WorldBoundaries): Food;
-export declare function isFood(object: GameObject): boolean;
